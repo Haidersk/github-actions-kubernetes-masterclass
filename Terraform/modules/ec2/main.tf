93b1a795167831b8a3caeaacf89b037ba8f4a6e8
Haider Shaikh <haidershaikh2050@gmail.com>
2026-05-22 21:23:38 +0530

    Add Jenkins module configuration in main.tf

---
+++ Terraform/modules/ec2/main.tf
@@ -0,0 +1,12 @@
+module "jenkins" {
+  source        = "../../modules/ec2"
+
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = data.aws_subnets.default.ids[0]
+
+  sg            = "sg-0c1d587650c0f8801"
+
+  key_name      = var.key_name
+  name           = "jenkins-server"
+}
